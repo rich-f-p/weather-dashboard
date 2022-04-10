1d82a171fb6abab31fecb6dffea5524573f6b630
rich p <richardferry3@gmail.com>
2022-04-09 22:23:00 -0400

    add function to fetch location api data

--- assets/script.js
+++ assets/script.js
@@ -2,20 +2,29 @@
 //forecast = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 const apikey = '8d31aa5ffe43cc6e2b64eb3cb37e4641';
 //long&lat = http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
-var name ='';
-var state ='';
-var country ='';
 const todayEl = $('#today');
 const futureEl = $('#future');
-var inputEl = $('#userInput')
+var inputEl = $('#userInput');
+var gpsLink = '';
 function search(event){
     event.preventDefault();
     locationUrl(inputEl.val());
+    gpsApi(gpsLink);
 }
 //generate url to longitude and latitude date
 function locationUrl(city){
     var longLat = 'http://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=5&appid='+apikey;
-    console.log(longLat);
+    gpsLink = longLat;
 }
+//fetch geolocation data
+function gpsApi(link) {
+    fetch(link)
+    .then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+    })
+}
+
 
 $('#search').on('submit',search);
