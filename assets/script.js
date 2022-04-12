1444e94d4e5a49139ae231f3c3fd5617c4a2a0a0
rich p <richardferry3@gmail.com>
2022-04-11 20:26:05 -0400

    removing comments not explaing code

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
-//http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}
-//forecast = https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
 const apikey = '8d31aa5ffe43cc6e2b64eb3cb37e4641';
-//long&lat = http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 const todayEl = $('#today');
 const futureEl = $('#future');
 var inputEl = $('#userInput');
